d55270530e76013e2f45871f9bcaafecd5773ad0
kalldaglls <kalldaglls@mail.ru>
2023-01-22 22:34:05 +0300

    ArchLes3DZFinalVersion

--- api/src/main/java/com/forgeeks/api/UserInfoDto.java
+++ api/src/main/java/com/forgeeks/api/UserInfoDto.java
@@ -1,20 +1,25 @@
 package com.forgeeks.api;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class UserInfoDto {
-    private String value;
+    private final String value;
 
-    public UserInfoDto() {
-    }
-
-    public UserInfoDto(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//    public UserInfoDto() {
+//    }
+//
+//    public UserInfoDto(String value) {
+//        this.value = value;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 }
